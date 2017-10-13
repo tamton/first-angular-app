4bb995bdb6456aee645ba5bb31580d75b9ccd6df
Tam Ton <tam.ton@apps-cyclone.com>
2017-10-13 10:52:59 +0700

    - first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Nghia Map. This is my first angular app';
 }
